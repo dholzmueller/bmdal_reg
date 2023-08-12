531abeeb27b9dc7f79743d3825a89e34208437c9
Hüseyin ARSLAN <71139213+h-arslan@users.noreply.github.com>
2023-08-12 14:31:48 +0300

    openml error fixed during downloading
    Error fixed on setting openml cache directory

--- bmdal_reg/download_data.py
+++ bmdal_reg/download_data.py
@@ -232,7 +232,7 @@ class PandasTask:
         :return: Returns a PandasTask representing the corresponding OpenML data set.
         """
         print(f'Importing dataset {ds_name}')
-        openml.config.set_cache_directory('./openml_cache')
+        openml.config.cache_directory = './openml_cache'
         dataset = openml.datasets.get_dataset(dataset_id, download_data=False)
         print(f'dataset name: {dataset.name}')
         # print(dataset.get_data(dataset_format='dataframe'))
